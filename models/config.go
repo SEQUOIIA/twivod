3aed7c1fd880b1015220d7a745d6351c4ea84038
Emil Hummel <sequoiia@hummel.yt>
2016-09-28 15:21:56 +0200

    Added placholder values for config data
    The previous client id & client secret that was commited is now listed as NOT IN USE.

--- models/config.go
+++ models/config.go
@@ -5,6 +5,6 @@ var TwitchConfig = struct {
 	Client_secret   string
 }{
 	// Insert the Client ID and secret that you created at http://www.twitch.tv/settings/connections
-	Client_id: "ifk71pkh6d1yjbw1g358tgh6m6cav78",
-	Client_secret: "bfegzk7v5i3ekxsupr88nfrrta5shn4",
+	Client_id: "INSERT YOUR CLIENT ID HERE",
+	Client_secret: "INSERT YOUR CLIENT SECRET HERE",
 }
