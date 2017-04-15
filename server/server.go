2e1d5a218797502357bd65f54a2895232f94d45f
Emil Hummel <emil@hummel.yt>
2017-04-16 00:30:03 +0200

    Moved Twitch client to package level variable

--- server/server.go
+++ server/server.go
@@ -6,20 +6,27 @@ import (
 	"fmt"
 )
 
+var T *twitch.Twitch
+
 func main() {
-	t := twitch.NewClient("whfl4lyxyzgp36d1el8v2yuyit0ge5")
-	t.Debug()
+	T = twitch.NewClient("whfl4lyxyzgp36d1el8v2yuyit0ge5")
+	T.Debug()
 
-	users, err := t.GetUsersByUsername([]string{"nalcs1", "sequoiia"})
-	if err != nil {
-		log.Fatal(err)
-	}
+	// Run in separate goroutine
+	go func() {
+		users, err := T.GetUsersByUsername([]string{"nalcs1", "sequoiia"})
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	for i := 0; i < len(users); i++ {
-		user := users[i]
-		fmt.Println(user.Name)
-	}
+		for i := 0; i < len(users); i++ {
+			user := users[i]
+			fmt.Println(user.Name)
+		}
+	}()
 
+
+	// Block primary goroutine
 	select {
 
 	}
