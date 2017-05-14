8271dd06dfcce4b44dae9bf29b2d1255df2afffe
Emil Hummel <emil@hummel.yt>
2017-05-14 21:20:45 +0200

    Refactor: Test data code moved to separate function
    The test data code initial resided in main(). Most of it has now been
    moved to debugData()

--- server/server.go
+++ server/server.go
@@ -19,51 +19,13 @@ func main() {
 
 	// Run in separate goroutine
 	go func() {
-		var userRaw []string = []string{"nalcs1", "sequoiia"}
-		var usersToGet []string = []string{}
-		var usersTotal []model.User
-
-		for i := 0; i < len(userRaw); i++ {
-			twitchUsername := userRaw[i]
-			u, err := T.Db.GetUserByTwitchUsername(twitchUsername)
-			if err != nil {
-				if err == db.ErrNoRow {
-					log.Println(fmt.Sprintf("Remote: %s", twitchUsername))
-					usersToGet = append(usersToGet, twitchUsername)
-					continue
-				} else {
-					log.Fatal(err)
-				}
-			}
-			log.Println(fmt.Sprintf("Db: %s", twitchUsername))
-			usersTotal = append(usersTotal, u)
-		}
-
-		// Get users that do not exist in DB
-
-		users, err := T.GetUsersByUsername(usersToGet)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// Add those users to the DB
-
-		for i := 0; i < len(users); i++ {
-			user := users[i]
-			err = T.Db.AddUser(user)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-
-		usersTotal = append(usersTotal, users...)
-
+		usersTotal := debugData()
 
 		// Print
 		for i := 0; i < len(usersTotal); i++ {
 			user := usersTotal[i]
 			fmt.Println(user.Name)
-			
+
 		}
 	}()
 
@@ -72,4 +34,47 @@ func main() {
 	select {
 
 	}
+}
+
+func debugData() []model.User{
+	var userRaw []string = []string{"nalcs1", "sequoiia"}
+	var usersToGet []string = []string{}
+	var usersTotal []model.User
+
+	for i := 0; i < len(userRaw); i++ {
+		twitchUsername := userRaw[i]
+		u, err := T.Db.GetUserByTwitchUsername(twitchUsername)
+		if err != nil {
+			if err == db.ErrNoRow {
+				log.Println(fmt.Sprintf("Remote: %s", twitchUsername))
+				usersToGet = append(usersToGet, twitchUsername)
+				continue
+			} else {
+				log.Fatal(err)
+			}
+		}
+		log.Println(fmt.Sprintf("Db: %s", twitchUsername))
+		usersTotal = append(usersTotal, u)
+	}
+
+	// Get users that do not exist in DB
+
+	users, err := T.GetUsersByUsername(usersToGet)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Add those users to the DB
+
+	for i := 0; i < len(users); i++ {
+		user := users[i]
+		err = T.Db.AddUser(user)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	usersTotal = append(usersTotal, users...)
+
+	return usersTotal
 }
