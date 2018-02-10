1422cfae283e3b4c985cf72ea40d98d95a24e247
Emil H. Clausen - Sequoiia <emil@hummel.yt>
2018-02-10 15:17:13 +0100

    feat: Added regex to support the changed Twitch url scheme

--- models/models.go
+++ models/models.go
@@ -1,191 +1,203 @@
 package models
 
 import (
-	"time"
 	"io"
+	"time"
 )
 
+// StreamType is a string type to help differentiate between live streams and VODs.
+type StreamType string
+
+// Livestream is a StreamType
+const Livestream StreamType = "livestream"
+
+// VOD is a StreamType
+const VOD StreamType = "videos"
+
+// Unknown is a StreamType
+const Unknown StreamType = "unknown"
+
 type VODinfo struct {
-	Channel		string
-	Type		string
-	ID			string
-	Url			string
+	Channel string
+	Type    string
+	ID      string
+	Url     string
 }
 
 type VODtypeB struct {
-	Chunks	struct {
-		Live	[]struct {
-			Url	string		`json:"url"`
-		}	`json:"live"`
-		Chunked	[]struct {
-			Url	string		`json:"url"`
-		}	`json:"chunked"`
-	}	`json:"chunks"`
+	Chunks struct {
+		Live []struct {
+			Url string `json:"url"`
+		} `json:"live"`
+		Chunked []struct {
+			Url string `json:"url"`
+		} `json:"chunked"`
+	} `json:"chunks"`
 }
 
 type TwitchOauthResponse struct {
-    AccessToken     string      `json:"access_token"`
-    RefreshToken    string      `json:"refresh_token"`
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
 }
 
 type HlsVodToken struct {
-    Token   string  `json:"token"`
-    Sig     string  `json:"sig"`
+	Token string `json:"token"`
+	Sig   string `json:"sig"`
 }
 
 type VodInfoKraken struct {
-	Title string `json:"title"`
-	Description interface{} `json:"description"`
-	BroadcastID int64 `json:"broadcast_id"`
-	BroadcastType string `json:"broadcast_type"`
-	Status string `json:"status"`
-	TagList string `json:"tag_list"`
-	Views int `json:"views"`
-	CreatedAt time.Time `json:"created_at"`
-	ID string `json:"_id"`
-	RecordedAt time.Time `json:"recorded_at"`
-	Game string `json:"game"`
-	Length float64 `json:"length"`
-	Preview string `json:"preview"`
-	Thumbnails []struct {
-		URL string `json:"url"`
+	Title         string      `json:"title"`
+	Description   interface{} `json:"description"`
+	BroadcastID   int64       `json:"broadcast_id"`
+	BroadcastType string      `json:"broadcast_type"`
+	Status        string      `json:"status"`
+	TagList       string      `json:"tag_list"`
+	Views         int         `json:"views"`
+	CreatedAt     time.Time   `json:"created_at"`
+	ID            string      `json:"_id"`
+	RecordedAt    time.Time   `json:"recorded_at"`
+	Game          string      `json:"game"`
+	Length        float64     `json:"length"`
+	Preview       string      `json:"preview"`
+	Thumbnails    []struct {
+		URL  string `json:"url"`
 		Type string `json:"type"`
 	} `json:"thumbnails"`
 	URL string `json:"url"`
 	Fps struct {
-		      AudioOnly float64 `json:"audio_only"`
-		      Medium float64 `json:"medium"`
-		      Mobile float64 `json:"mobile"`
-		      High float64 `json:"high"`
-		      Low float64 `json:"low"`
-		      Chunked float64 `json:"chunked"`
-	      } `json:"fps"`
+		AudioOnly float64 `json:"audio_only"`
+		Medium    float64 `json:"medium"`
+		Mobile    float64 `json:"mobile"`
+		High      float64 `json:"high"`
+		Low       float64 `json:"low"`
+		Chunked   float64 `json:"chunked"`
+	} `json:"fps"`
 	Resolutions struct {
-		      Medium string `json:"medium"`
-		      Mobile string `json:"mobile"`
-		      High string `json:"high"`
-		      Low string `json:"low"`
-		      Chunked string `json:"chunked"`
-	      } `json:"resolutions"`
+		Medium  string `json:"medium"`
+		Mobile  string `json:"mobile"`
+		High    string `json:"high"`
+		Low     string `json:"low"`
+		Chunked string `json:"chunked"`
+	} `json:"resolutions"`
 	Links struct {
-		      Self string `json:"self"`
-		      Channel string `json:"channel"`
-	      } `json:"_links"`
+		Self    string `json:"self"`
+		Channel string `json:"channel"`
+	} `json:"_links"`
 	Channel struct {
-		      Name string `json:"name"`
-		      DisplayName string `json:"display_name"`
-	      } `json:"channel"`
+		Name        string `json:"name"`
+		DisplayName string `json:"display_name"`
+	} `json:"channel"`
 }
 
 type TwitchUser struct {
-	DisplayName string `json:"display_name"`
-	ID int `json:"_id"`
-	Name string `json:"name"`
-	Type string `json:"type"`
-	Bio string `json:"bio"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	Logo string `json:"logo"`
-	Links struct {
-			    Self string `json:"self"`
-		    } `json:"_links"`
+	DisplayName string    `json:"display_name"`
+	ID          int       `json:"_id"`
+	Name        string    `json:"name"`
+	Type        string    `json:"type"`
+	Bio         string    `json:"bio"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	Logo        string    `json:"logo"`
+	Links       struct {
+		Self string `json:"self"`
+	} `json:"_links"`
 }
 
 type TwitchUserVods struct {
 	Total int `json:"_total"`
 	Links struct {
-		      Self string `json:"self"`
-		      Next string `json:"next"`
-	      } `json:"_links"`
+		Self string `json:"self"`
+		Next string `json:"next"`
+	} `json:"_links"`
 	Videos []struct {
-		Title string `json:"title"`
-		Description interface{} `json:"description"`
-		BroadcastID int64 `json:"broadcast_id"`
-		BroadcastType string `json:"broadcast_type"`
-		Status string `json:"status"`
-		TagList string `json:"tag_list"`
-		Views int `json:"views"`
-		CreatedAt time.Time `json:"created_at"`
-		ID string `json:"_id"`
-		RecordedAt time.Time `json:"recorded_at"`
-		Game string `json:"game"`
-		Length int `json:"length"`
-		Preview string `json:"preview"`
-		Thumbnails []struct {
-			URL string `json:"url"`
+		Title         string      `json:"title"`
+		Description   interface{} `json:"description"`
+		BroadcastID   int64       `json:"broadcast_id"`
+		BroadcastType string      `json:"broadcast_type"`
+		Status        string      `json:"status"`
+		TagList       string      `json:"tag_list"`
+		Views         int         `json:"views"`
+		CreatedAt     time.Time   `json:"created_at"`
+		ID            string      `json:"_id"`
+		RecordedAt    time.Time   `json:"recorded_at"`
+		Game          string      `json:"game"`
+		Length        int         `json:"length"`
+		Preview       string      `json:"preview"`
+		Thumbnails    []struct {
+			URL  string `json:"url"`
 			Type string `json:"type"`
 		} `json:"thumbnails"`
 		URL string `json:"url"`
 		Fps struct {
-			      AudioOnly float64 `json:"audio_only"`
-			      Medium float64 `json:"medium"`
-			      Mobile float64 `json:"mobile"`
-			      High float64 `json:"high"`
-			      Low float64 `json:"low"`
-			      Chunked float64 `json:"chunked"`
-		      } `json:"fps"`
+			AudioOnly float64 `json:"audio_only"`
+			Medium    float64 `json:"medium"`
+			Mobile    float64 `json:"mobile"`
+			High      float64 `json:"high"`
+			Low       float64 `json:"low"`
+			Chunked   float64 `json:"chunked"`
+		} `json:"fps"`
 		Resolutions struct {
-			      Medium string `json:"medium"`
-			      Mobile string `json:"mobile"`
-			      High string `json:"high"`
-			      Low string `json:"low"`
-			      Chunked string `json:"chunked"`
-		      } `json:"resolutions"`
+			Medium  string `json:"medium"`
+			Mobile  string `json:"mobile"`
+			High    string `json:"high"`
+			Low     string `json:"low"`
+			Chunked string `json:"chunked"`
+		} `json:"resolutions"`
 		Links struct {
-			      Self string `json:"self"`
-			      Channel string `json:"channel"`
-		      } `json:"_links"`
+			Self    string `json:"self"`
+			Channel string `json:"channel"`
+		} `json:"_links"`
 		Channel struct {
-			      Name string `json:"name"`
-			      DisplayName string `json:"display_name"`
-		      } `json:"channel"`
+			Name        string `json:"name"`
+			DisplayName string `json:"display_name"`
+		} `json:"channel"`
 	} `json:"videos"`
 }
 
 type TwitchUsersSearch struct {
 	Total int `json:"_total"`
 	Links struct {
-		      Self string `json:"self"`
-		      Next string `json:"next"`
-	      } `json:"_links"`
+		Self string `json:"self"`
+		Next string `json:"next"`
+	} `json:"_links"`
 	Channels []struct {
-		Mature bool `json:"mature"`
-		Status string `json:"status"`
-		BroadcasterLanguage string `json:"broadcaster_language"`
-		DisplayName string `json:"display_name"`
-		Game string `json:"game"`
-		Language string `json:"language"`
-		ID int `json:"_id"`
-		Name string `json:"name"`
-		CreatedAt time.Time `json:"created_at"`
-		UpdatedAt time.Time `json:"updated_at"`
-		Delay interface{} `json:"delay"`
-		Logo string `json:"logo"`
-		Banner interface{} `json:"banner"`
-		VideoBanner string `json:"video_banner"`
-		Background interface{} `json:"background"`
-		ProfileBanner string `json:"profile_banner"`
+		Mature                       bool        `json:"mature"`
+		Status                       string      `json:"status"`
+		BroadcasterLanguage          string      `json:"broadcaster_language"`
+		DisplayName                  string      `json:"display_name"`
+		Game                         string      `json:"game"`
+		Language                     string      `json:"language"`
+		ID                           int         `json:"_id"`
+		Name                         string      `json:"name"`
+		CreatedAt                    time.Time   `json:"created_at"`
+		UpdatedAt                    time.Time   `json:"updated_at"`
+		Delay                        interface{} `json:"delay"`
+		Logo                         string      `json:"logo"`
+		Banner                       interface{} `json:"banner"`
+		VideoBanner                  string      `json:"video_banner"`
+		Background                   interface{} `json:"background"`
+		ProfileBanner                string      `json:"profile_banner"`
 		ProfileBannerBackgroundColor interface{} `json:"profile_banner_background_color"`
-		Partner bool `json:"partner"`
-		URL string `json:"url"`
-		Views int `json:"views"`
-		Followers int `json:"followers"`
-		Links struct {
-			       Self string `json:"self"`
-			       Follows string `json:"follows"`
-			       Commercial string `json:"commercial"`
-			       StreamKey string `json:"stream_key"`
-			       Chat string `json:"chat"`
-			       Features string `json:"features"`
-			       Subscriptions string `json:"subscriptions"`
-			       Editors string `json:"editors"`
-			       Teams string `json:"teams"`
-			       Videos string `json:"videos"`
-		       } `json:"_links"`
+		Partner                      bool        `json:"partner"`
+		URL                          string      `json:"url"`
+		Views                        int         `json:"views"`
+		Followers                    int         `json:"followers"`
+		Links                        struct {
+			Self          string `json:"self"`
+			Follows       string `json:"follows"`
+			Commercial    string `json:"commercial"`
+			StreamKey     string `json:"stream_key"`
+			Chat          string `json:"chat"`
+			Features      string `json:"features"`
+			Subscriptions string `json:"subscriptions"`
+			Editors       string `json:"editors"`
+			Teams         string `json:"teams"`
+			Videos        string `json:"videos"`
+		} `json:"_links"`
 	} `json:"channels"`
 }
 
 type TwitchVodSegment struct {
-	Id int
+	Id           int
 	ResponseBody io.ReadCloser
 }
--- utilities/parser/parser.go
+++ utilities/parser/parser.go
@@ -2,8 +2,10 @@ package parser
 
 import (
 	"fmt"
-	"github.com/sequoiia/twiVod/models"
 	"regexp"
+	"strings"
+
+	"github.com/sequoiia/twiVod/models"
 )
 
 func VodInfo(url string) models.VODinfo {
@@ -16,4 +18,40 @@ func VodInfo(url string) models.VODinfo {
 		vod := models.VODinfo{Type: "404"}
 		return vod
 	}
+}
+
+// DetermineStreamType runs a regex expression that determines whether a live stream or a VOD has been given
+func DetermineStreamType(url string) models.StreamType {
+	r := regexp.MustCompile(`(?:https?:\/\/)?(?:www\.)?twitch.tv\/(videos\/(?P<videos>\w+)|(?P<livestream>\w+))`)
+	if r.MatchString(url) {
+		match := r.FindStringSubmatch(url)
+		resMapped := make(map[string]string)
+
+		for key, val := range r.SubexpNames() {
+			if key != 0 && strings.Compare(val, "") != 0 {
+				if strings.Compare(match[key], "") != 0 {
+					resMapped[val] = match[key]
+				}
+			}
+		}
+
+		if len(resMapped) != 0 {
+			for k, v := range resMapped {
+				//fmt.Printf("%s: %s\n", k, v)
+				switch k {
+				case "livestream":
+					fmt.Printf("Live stream detected. Looking up %s\n", v)
+					return models.Livestream
+				case "videos":
+					fmt.Println("VOD detected.")
+					return models.VOD
+				}
+			}
+		} else {
+			fmt.Println("No match found.")
+		}
+
+	}
+
+	return models.Unknown
 }
