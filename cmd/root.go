114ee6fe6d6c0886433084b405c2b99299b19c20
Emil H. Clausen - Sequoiia <emil@hummel.yt>
2018-02-11 21:19:57 +0100

    feat: Store client id in config file

--- cmd/root.go
+++ cmd/root.go
@@ -10,25 +10,43 @@ import (
 	"github.com/sequoiia/twivod/models"
 	"github.com/sequoiia/twivod/utilities/downloader"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var (
-	Verbose bool
+	Verbose             bool
 	ConcurrentDownloads int
-	BandwidthLimit int64
-	PrintVersion bool
+	BandwidthLimit      int64
+	PrintVersion        bool
+	SetTwitchClientID   string
+	TwitchClientID      string
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "twivod https://www.twitch.tv/videos/227241536",
 	Short: "twiVod is a simple tool for downloading Twitch VODs and live streams.",
-	Long: `twiVod is a simple tool for downloading Twitch VODs and live streams.`,
+	Long:  `twiVod is a simple tool for downloading Twitch VODs and live streams.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		vodOptions := &models.TwitchVodOptions{Url: args[0], MaxConcurrentDownloads: ConcurrentDownloads}
 
 		if BandwidthLimit != math.MaxInt64 {
 			BandwidthLimit = BandwidthLimit * 1000
 		}
+
+		if viper.Sub("twitch").GetString("clientid") == "undefined" {
+			if TwitchClientID == "" {
+				fmt.Println("Twitch client-id has not been set. Run 'twivod --help' for further help.")
+				os.Exit(1)
+			}
+
+		} else {
+			models.TwitchConfig.Client_id = viper.GetString("twitchclientid")
+		}
+
+		if TwitchClientID != "" {
+			models.TwitchConfig.Client_id = TwitchClientID
+		}
+
 		err := downloader.Download(vodOptions, BandwidthLimit)
 		if err != nil {
 			log.Fatal(err)
@@ -36,14 +54,24 @@ var rootCmd = &cobra.Command{
 
 		downloader.Remux(vodOptions)
 	},
-	Args: func(cmd *cobra.Command, args[]string) error{
+	Args: func(cmd *cobra.Command, args []string) error {
 		if PrintVersion {
 			versionCmd.Run(cmd, args)
 			os.Exit(1)
 		}
 
+		if SetTwitchClientID != "" {
+			viper.Set("twitchclientid", SetTwitchClientID)
+			err := viper.WriteConfig()
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println("Client-ID saved in config.")
+			os.Exit(1)
+		}
+
 		if len(args) < 1 {
-			return errors.New("Requires at least one arg(url)")
+			return errors.New("requires at least one arg(url)")
 		}
 		return nil
 	},
@@ -61,9 +89,11 @@ func Execute() {
 func Init() {
 	// rootCmd
 	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "Verbose output")
+	rootCmd.PersistentFlags().BoolVar(&PrintVersion, "version", false, "Print software version")
 	rootCmd.PersistentFlags().IntVarP(&ConcurrentDownloads, "concurrent-download", "c", 1, "The amount of video segments downloaded at the same time.")
 	rootCmd.PersistentFlags().Int64VarP(&BandwidthLimit, "bwlimit", "b", math.MaxInt64, "Limits download speed, value is in kb/s")
-	rootCmd.PersistentFlags().BoolVar(&PrintVersion, "version", false, "Print software version")
+	rootCmd.PersistentFlags().StringVar(&SetTwitchClientID, "set-client-id", "", "Set client-id in config")
+	rootCmd.PersistentFlags().StringVar(&TwitchClientID, "client-id", "", "Use client-id for only this command")
 
 	rootCmd.AddCommand(versionCmd)
 }
--- main.go
+++ main.go
@@ -1,9 +1,42 @@
 package main
 
 import (
+	"io/ioutil"
+	"log"
+	"os"
+	"runtime"
+
 	"github.com/sequoiia/twivod/cmd"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	viper.SetConfigName("twivod")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("$HOME/.config")
+	viper.AddConfigPath("$APPDATA/twivod")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		defaultConfig := []byte("twitchclientid: undefined\n")
+		if runtime.GOOS == "windows" {
+			os.Mkdir("$APPDATA/twivod", 0770)
+			err := ioutil.WriteFile("$APPDATA/twivod/twivod.yaml", defaultConfig, 0770)
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			err := ioutil.WriteFile(os.Getenv("HOME")+"/.config/twivod.yaml", defaultConfig, 0770)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		err = viper.ReadInConfig()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
 	cmd.Execute()
 }
